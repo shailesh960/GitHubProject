e9d3e3de3f21ad956e6b2a736b39bacf1a6c5e23
om <om@DESKTOP-HSN9QIK>
2022-01-01 15:24:10 -0500

    commit new project

---
+++ gitHubProject/src/test/java/createGithubPro/CreateGitHubId.java
@@ -0,0 +1,14 @@
+package createGithubPro;
+
+import org.testng.annotations.Test;
+
+public class CreateGitHubId 
+{
+	@Test
+	public void createId()
+	{
+		System.out.println("Enter Id");
+	}
+	
+
+}
